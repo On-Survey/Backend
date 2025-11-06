ef067e15c8a842c61bac21036a6b26f46fbf2277
shash0423 <shash042319@naver.com>
2025-11-06 13:56:38 +0900

    chore: 앱인토스 CORS 주소 추가

--- src/main/java/OneQ/OnSurvey/global/config/CorsConfig.java
+++ src/main/java/OneQ/OnSurvey/global/config/CorsConfig.java
@@ -21,7 +21,9 @@ public class CorsConfig {
         config.setAllowedOrigins(Arrays.asList(
                 "http://localhost:5173", //Todo 배포시 삭제할 것들 확인
                 "http://localhost:8080",
-                "https://api.onsurvey.co.kr"
+                "https://api.onsurvey.co.kr",
+                "https://onsurvey.private-apps.tossmini.com",
+                "https://onsurvey.apps.tossmini.com"
         ));
         config.setAllowedMethods(Arrays.asList(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
