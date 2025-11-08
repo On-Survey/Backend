3fd3bf82b4de40b3446e9fae5d6d44fe7c42e976
shash0423 <shash042319@naver.com>
2025-11-08 17:38:00 +0900

    chore: surveyId 하드코딩 제거 #95

--- src/main/java/OneQ/OnSurvey/global/infra/toss/controller/PromotionController.java
+++ src/main/java/OneQ/OnSurvey/global/infra/toss/controller/PromotionController.java
@@ -2,13 +2,16 @@ package OneQ.OnSurvey.global.infra.toss.controller;
 
 import OneQ.OnSurvey.global.auth.custom.CustomUserDetails;
 import OneQ.OnSurvey.global.infra.toss.dto.ExecutionResultResponse;
+import OneQ.OnSurvey.global.infra.toss.dto.PromotionIssueRequest;
 import OneQ.OnSurvey.global.infra.toss.service.PromotionService;
 import OneQ.OnSurvey.global.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,10 +29,10 @@ public class PromotionController {
             description = "프로모션 코드/금액으로 토스 포인트 지급을 실행합니다."
     )
     public SuccessResponse<ExecutionResultResponse> issue(
-            @AuthenticationPrincipal CustomUserDetails principal
-            // TODO: 설문 연결 후 실제 surveyId 입력받아야 함
+            @AuthenticationPrincipal CustomUserDetails principal,
+            @RequestBody @Valid PromotionIssueRequest request
     ) {
-        ExecutionResultResponse res = promotionService.issueAndConfirm(principal.getUserKey(), 1L);
+        ExecutionResultResponse res = promotionService.issueAndConfirm(principal.getUserKey(), request.surveyId());
         return SuccessResponse.ok(res);
     }
 }
