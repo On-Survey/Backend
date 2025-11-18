5c167814db5b19883ce54073bcb5403a6003680e
wonjuneee <tony000709@gmail.com>
2025-11-18 22:17:47 +0900

    fix: 파라미터가 없는 기본 생성자 추가

--- src/main/java/OneQ/OnSurvey/domain/participation/entity/id/ResponseId.java
+++ src/main/java/OneQ/OnSurvey/domain/participation/entity/id/ResponseId.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Getter @Builder
 @EqualsAndHashCode
 @AllArgsConstructor
+@NoArgsConstructor
 public class ResponseId implements Serializable {
     private Long surveyId;
     private Long memberId;
