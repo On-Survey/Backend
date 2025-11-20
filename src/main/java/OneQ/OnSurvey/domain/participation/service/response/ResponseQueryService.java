00feaf5770fa85b0546ab4de0ece407abe9920dd
wonjuneee <tony000709@gmail.com>
2025-11-21 01:50:46 +0900

    mod: 응답자 수 계산 로직 개선

--- src/main/java/OneQ/OnSurvey/domain/participation/service/response/ResponseQueryService.java
+++ src/main/java/OneQ/OnSurvey/domain/participation/service/response/ResponseQueryService.java
@@ -7,7 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -33,8 +35,15 @@ public class ResponseQueryService implements ResponseQuery {
 
     @Override
     public Integer getResponseCountBySurveyId(Long surveyId) {
-        log.info("[MANAGEMENT:RESPONSE_SERVICE] 상세조회 설문 응답 수 - surveyId: {}", surveyId);
+        log.info("[RESPONSE:QUERY:getResponseCountBySurveyId] 설문 응답 수 - surveyId: {}", surveyId);
 
         return responseRepository.getResponseCountBySurveyId(surveyId);
+    }
+
+    @Override
+    public Map<Long, Long> getResponseCountsBySurveyIds(Collection<Long> surveyIds) {
+        log.info("[RESPONSE:QUERY:getResponseCountsBySurveyIds] 설문 응답 수 일괄 조회 - surveyIds: {}", surveyIds);
+
+        return responseRepository.getResponseCountsBySurveyIds(surveyIds);
     }
 }
