834d9814005608cd5a23268b5b1ed94c0135accd
shash0423 <shash042319@naver.com>
2025-11-27 01:40:06 +0900

    refactor: 사용하지 않는 메서드 정리

--- src/main/java/OneQ/OnSurvey/domain/participation/service/response/ResponseQuery.java
+++ src/main/java/OneQ/OnSurvey/domain/participation/service/response/ResponseQuery.java
@@ -1,18 +1,11 @@
 package OneQ.OnSurvey.domain.participation.service.response;
 
-import OneQ.OnSurvey.domain.participation.entity.Response;
 import OneQ.OnSurvey.domain.survey.model.SurveyResponseFilterCondition;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 
 public interface ResponseQuery {
-    List<Response> getResponsesByMemberId(Long memberId);
-    List<Response> getResponsesBySurveyId(Long surveyId);
-
-    Response getResponseBySurveyIdAndMemberId(Long surveyId, Long memberId);
-
     Integer getResponseCountBySurveyId(Long surveyId);
     default Integer getResponseCountBySurveyId(
             Long surveyId,
--- src/main/java/OneQ/OnSurvey/domain/participation/service/response/ResponseQueryService.java
+++ src/main/java/OneQ/OnSurvey/domain/participation/service/response/ResponseQueryService.java
@@ -1,6 +1,5 @@
 package OneQ.OnSurvey.domain.participation.service.response;
 
-import OneQ.OnSurvey.domain.participation.entity.Response;
 import OneQ.OnSurvey.domain.participation.repository.response.ResponseRepository;
 import OneQ.OnSurvey.domain.survey.model.SurveyResponseFilterCondition;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -17,22 +15,8 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ResponseQueryService implements ResponseQuery {
+
     private final ResponseRepository responseRepository;
-
-    @Override
-    public List<Response> getResponsesByMemberId(Long memberId) {
-        return responseRepository.getResponsesByMemberId(memberId);
-    }
-
-    @Override
-    public List<Response> getResponsesBySurveyId(Long surveyId) {
-        return responseRepository.getResponsesBySurveyId(surveyId);
-    }
-
-    @Override
-    public Response getResponseBySurveyIdAndMemberId(Long surveyId, Long memberId) {
-        return responseRepository.getResponseBySurveyIdAndMemberId(surveyId, memberId);
-    }
 
     @Override
     public Integer getResponseCountBySurveyId(Long surveyId) {
