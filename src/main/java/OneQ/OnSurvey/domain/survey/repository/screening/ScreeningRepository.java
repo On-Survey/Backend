ad3a4e5255879492806c0eb6a0b7ca24ddfdcf9a
wonjuneee <tony000709@gmail.com>
2026-01-17 05:32:56 +0900

    mod: 설문 수정 시 스크리닝 데이터도 추가로 반환하도록 수정

--- src/main/java/OneQ/OnSurvey/domain/survey/repository/screening/ScreeningRepository.java
+++ src/main/java/OneQ/OnSurvey/domain/survey/repository/screening/ScreeningRepository.java
@@ -1,6 +1,7 @@
 package OneQ.OnSurvey.domain.survey.repository.screening;
 
 import OneQ.OnSurvey.domain.survey.entity.Screening;
+import OneQ.OnSurvey.domain.survey.model.dto.ScreeningFormData;
 import OneQ.OnSurvey.domain.survey.model.dto.ScreeningIntroData;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 public interface ScreeningRepository {
     Screening getScreeningBySurveyId(Long surveyId);
     List<ScreeningIntroData> getScreeningListBySurveyIdList(List<Long> surveyIdList);
+    ScreeningFormData getScreeningFormDataBySurveyId(Long surveyId);
 
     Boolean getScreeningAnswer(Long screeningId);
     Long getSurveyId(Long screeningId);
