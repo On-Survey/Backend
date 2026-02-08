07782c4c5ac45faf3510508b4c1508be69b7220a
shash0423 <shash042319@naver.com>
2026-02-08 12:44:04 +0900

    chore: GCM 태그 길이 고려

--- src/main/java/OneQ/OnSurvey/global/auth/filter/TossAuthFilter.java
+++ src/main/java/OneQ/OnSurvey/global/auth/filter/TossAuthFilter.java
@@ -49,6 +49,7 @@ public class TossAuthFilter extends OncePerRequestFilter {
                 || path.equals("/swagger-ui.html")
                 || path.startsWith("/swagger-ui/")
                 || path.startsWith("/v3/api-docs")
+                || path.startsWith("/")
                 || path.equals("/toss/promotion/recheck-pending");
     }
 
