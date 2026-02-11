cdae862b5b873c2bc13da3d3d2ca5fb7a204e34c
wonjuneee <tony000709@gmail.com>
2026-02-11 13:36:20 +0900

    feat: 설문변환요청 검색 필터링 로직 구현

--- src/main/java/OneQ/OnSurvey/domain/survey/repository/formRequest/FormRequestRepository.java
+++ src/main/java/OneQ/OnSurvey/domain/survey/repository/formRequest/FormRequestRepository.java
@@ -1,6 +1,8 @@
 package OneQ.OnSurvey.domain.survey.repository.formRequest;
 
 import OneQ.OnSurvey.domain.survey.entity.FormRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +11,5 @@ public interface FormRequestRepository {
     FormRequest save(FormRequest request);
     Optional<FormRequest> findById(Long id);
     List<FormRequest> findAllUnregistered();
+    Page<FormRequest> findAllWithFilters(String email, Boolean isRegistered, Pageable pageable);
 }
