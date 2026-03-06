56bc0ccbe9c03d62338df4f576196b5cc492f9bd
wonjuneee <tony000709@gmail.com>
2026-03-06 19:46:46 +0900

    fix: RedisConfig 수정 및 누락된 트랜잭션 어노테이션 추가

--- src/main/java/OneQ/OnSurvey/global/common/config/RedisConfig.java
+++ src/main/java/OneQ/OnSurvey/global/common/config/RedisConfig.java
@@ -4,29 +4,21 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
-    @Value("${spring.data.redis.password:}")
-    private String password;
-
     @Bean
-    public RedissonClient redisson() {
+    public RedissonClient redisson(RedisProperties redisProperties) {
         Config config = new Config();
+
         SingleServerConfig singleServerConfig = config.useSingleServer()
-            .setAddress("redis://" + host + ":" + port);
-        if (password != null && !password.isBlank()) {
-            singleServerConfig.setPassword(password);
+            .setAddress(String.format("redis://%s:%d", redisProperties.getHost(), redisProperties.getPort()));
+        if (redisProperties.getPassword() != null && !redisProperties.getPassword().isBlank()) {
+            singleServerConfig.setPassword(redisProperties.getPassword());
         }
 
         return Redisson.create(config);
