4fb603359469ad57fe808140a121cc69f5cb58dc
jaekwan <jaegwan101@gmail.com>
2026-03-21 13:57:53 +0900

    fix: dto에 validation 추가

--- src/main/java/OneQ/OnSurvey/domain/discount/model/request/CreateDiscountCodeRequest.java
+++ src/main/java/OneQ/OnSurvey/domain/discount/model/request/CreateDiscountCodeRequest.java
@@ -1,6 +1,7 @@
 package OneQ.OnSurvey.domain.discount.model.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -10,7 +11,9 @@ public record CreateDiscountCodeRequest(
         @NotBlank
         @Schema(description = "학회(기관) 이름", example = "onsurvey")
         String organizationName,
+
         @NotNull
+        @FutureOrPresent
         @Schema(description = "코드 만료 기한", example = "2026-12-31")
         LocalDate expiredAt
 ) {
--- src/main/java/OneQ/OnSurvey/domain/survey/model/formRequest/FormRequestDto.java
+++ src/main/java/OneQ/OnSurvey/domain/survey/model/formRequest/FormRequestDto.java
@@ -2,12 +2,17 @@ package OneQ.OnSurvey.domain.survey.model.formRequest;
 
 import OneQ.OnSurvey.domain.survey.entity.FormRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record FormRequestDto(
         @Schema(description = "폼 링크", example = "https://docs.google.com/forms/d/e/1FAIpQLSfD.../viewform")
+        @NotBlank
         String formLink,
 
         @Schema(description = "신청자 이메일", example = "test@gmail.com")
+        @Email
+        @NotBlank
         String requesterEmail
 ) {
     public FormRequest toEntity() {
