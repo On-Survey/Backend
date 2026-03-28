657f206be14efaa8f98b3b7ea99b89b2c6131bb5
wonjuneee <tony000709@gmail.com>
2026-03-28 23:51:17 +0900

    fix: 람다 응답 필드명 오타 수정

--- src/main/java/OneQ/OnSurvey/domain/survey/model/formRequest/FormValidationPostResponse.java
+++ src/main/java/OneQ/OnSurvey/domain/survey/model/formRequest/FormValidationPostResponse.java
@@ -31,7 +31,7 @@ public record FormValidationPostResponse(
     public record Count(
         int total,
         int convertible,
-        int unconvertible
+        int inconvertible
     ) { }
 
     public record Inconvertible(
