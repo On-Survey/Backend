daa763d35e7fa088631dde0b379a9d4193da0a75
shash0423 <shash042319@naver.com>
2026-04-29 15:34:14 +0900

    hotfix: 연령대 계산 불일치 해결 (만나이, 한국나이)

--- src/main/java/OneQ/OnSurvey/domain/participation/repository/response/ResponseRepositoryImpl.java
+++ src/main/java/OneQ/OnSurvey/domain/participation/repository/response/ResponseRepositoryImpl.java
@@ -140,7 +140,7 @@ public class ResponseRepositoryImpl implements ResponseRepository {
         }
 
         return Expressions.booleanTemplate(
-                "(YEAR(CURDATE()) - CAST(SUBSTRING({0}, 1, 4) AS long)) BETWEEN {1} AND {2}",
+                "(YEAR(CURDATE()) - CAST(SUBSTRING({0}, 1, 4) AS long) + 1) BETWEEN {1} AND {2}",
                 birthDayPath, minAge, maxAge
         );
     }
