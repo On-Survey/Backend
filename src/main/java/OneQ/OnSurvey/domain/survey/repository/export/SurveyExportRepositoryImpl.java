583806d0736d39ce3e998f963dc16953f928f3c0
shash0423 <shash042319@naver.com>
2026-05-06 16:59:14 +0900

    fix: CSV 추출 시 응답자 행 순서를 제출 시점(createdAt) 기준으로 정렬

--- src/main/java/OneQ/OnSurvey/domain/survey/repository/export/SurveyExportRepositoryImpl.java
+++ src/main/java/OneQ/OnSurvey/domain/survey/repository/export/SurveyExportRepositoryImpl.java
@@ -60,7 +60,7 @@ public class SurveyExportRepositoryImpl implements SurveyExportRepository {
                 .join(response).on(response.surveyId.eq(surveyId).and(response.memberId.eq(questionAnswer.memberId)))
                 .where(question.surveyId.eq(surveyId).and(response.isResponded.isTrue()))
                 .distinct()
-                .orderBy(member.id.asc())
+                .orderBy(response.createdAt.asc())
                 .fetch();
     }
 
@@ -78,7 +78,7 @@ public class SurveyExportRepositoryImpl implements SurveyExportRepository {
                 .join(question).on(question.questionId.eq(questionAnswer.questionId))
                 .join(response).on(response.surveyId.eq(surveyId).and(response.memberId.eq(questionAnswer.memberId)))
                 .where(question.surveyId.eq(surveyId).and(response.isResponded.isTrue()))
-                .orderBy(response.updatedAt.asc(), question.order.asc(), questionAnswer.gridRowOrder.asc())
+                .orderBy(response.createdAt.asc(), question.order.asc(), questionAnswer.gridRowOrder.asc())
                 .fetch();
     }
 
